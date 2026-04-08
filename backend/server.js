018e2ddbd6dd3f9049c18314745eeee6b49bb58a
Mustafa Shaikh <mustafashaikh6634@gmail.com>
2026-04-08 17:55:09 +0530

    updated server.js

--- backend/server.js
+++ backend/server.js
@@ -28,8 +28,10 @@ app.get("/", (req, res) => {
   res.send("API is running...");
 });
 
+const PORT = 5000;
 
-app.listen(5000, () => {
-  console.log(`Server running on port 5000`);
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
     connectDB();
 });
